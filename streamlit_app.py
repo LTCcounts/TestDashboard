a947952c0fab6e51f125c5be9d643cc02d90dfce
LTCcounts <stashalseiu2015@gmail.com>
2026-02-07 00:04:43 +0000

    text

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ with st.expander('At-Large Finances'):
 with st.expander('Chapter Account Balances'):
     #st.header("About the dashboard:")
     st.write("These records reflect account balances presented at monthly e-board meetings, and so offer a snapshot of account balances over time.")
-    st.write("Last updated: 2/6/2026")
+    st.write("Last updated: 1/22/2026")
     st.dataframe(df_filtered.set_index(df.columns[0]))
   
     col1, col2 = st.columns(2)
